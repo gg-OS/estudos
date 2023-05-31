78c9be254b80800e8c7f1b3e69803c81f923acd4
Guilherme <114688336+gg-OS@users.noreply.github.com>
2023-05-31 01:13:01 -0300

    Delete exercicios (extra)/listas PythonBrasil/lista4 (vetores e listas) directory

--- exercicios (extra)/listas PythonBrasil/lista4 (vetores e listas)/exercicio24.py
+++
@@ -1,9 +0,0 @@
-import random
-c = 0
-dado = [0, 0, 0, 0, 0, 0]
-while c < 100:
-    x = random.randint(1, 6)
-    dado[x - 1] += 1
-    c += 1
-for n in range(1, 7):
-    print(f'O valor {n} apareceu {dado[n - 1]} vezes.')
