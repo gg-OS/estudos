94979f3f01e2be95b82f065d364d80c59077daef
Guilherme <114688336+gg-OS@users.noreply.github.com>
2023-06-20 18:10:07 -0300

    Update Heap Sort.py

--- Faculdade/Estrutura de Dados/Trabalho final/Heap Sort.py
+++ Faculdade/Estrutura de Dados/Trabalho final/Heap Sort.py
@@ -1,128 +1,46 @@
-# Classe para representar o produto
-class Produto:
-
-    def __init__(self, nome, valor, next=None):
-        self.nome = nome
-        self.valor = valor
-        self.next = next
+def troca(lista, i, j):
+    lista[i], lista[j] = lista[j], lista[i]
 
 
-# Lista encadeada para inserir os produtos
-class ListaDeProdutos:
-
-    def __init__(self):
-        self.head = None
-
-    # Método de inserção de produtos na lista
-    def inserir(self, nome, valor):
-        new_product = Produto(nome, valor)
-        if self.head is None:
-            self.head = new_product
-        else:
-            itr = self.head
-            while itr.next:
-                itr = itr.next
-            itr.next = new_product
-
-    # Método para exibir produtos listados
-    def exibir(self):
-        if self.head is None:
-            print('Lista de produtos vazia')
-            return
-        itr = self.head
-        lista = ''
-        while itr:
-            lista += '(' + str(itr.nome) + ': ' + str(itr.valor) + ')' + ', '
-            itr = itr.next
-        return lista
-
-    # Método para retornar tamanho da lista encadeada
-    def tamanho(self):
-        itr = self.head
-        c = 0
-        while itr:
-            itr = itr.next
-            c += 1
-        return c
-
-    # Método para retornar um produto de determinado índice
-    def retornar_valor_indice(self, indice):
-        itr = self.head
-        if indice - 1 > self.tamanho():
-            print('Índice inválido')
-            return
-        for i in range(0, indice):
-            itr = itr.next
-        return itr.valor
-
-
-# Classe da árvore
-class Node:
-
-    def __init__(self, produto, esquerda=None, direita=None):
-        self.produto = produto
-        self.esquerda = esquerda
-        self.direita = direita
-
-    # Método para inserir produto na árvore
-    def inserir(self, produto):
-        if self.produto:
-            if produto < self.produto:
-                if self.esquerda is None:
-                    self.esquerda = Node(produto)
-                else:
-                    self.esquerda.inserir(produto)
-            elif produto > self.produto:
-                if self.direita is None:
-                    self.direita = Node(produto)
-                else:
-                    self.direita.inserir(produto)
+def braco(lista, i, acima):
+    while True:
+        l, r = i * 2 + 1, i * 2 + 2
+        if max(l, r) < acima:
+            if lista[i] >= max(lista[l], lista[r]):
+                break
+            elif lista[l] > lista[r]:
+                troca(lista, i, l)
+                i = l
             else:
-                self.produto = produto
-
-# Função de construir a árvore
-    def construcao(self):
-        pass
-
-# Função de extrair elementos da árvore para ordenar o heap
-    def extracao(self):
-        pass
-
-
-def em_ordem(raiz):
-    if not raiz:
-        return
-
-    # Visita filho da esquerda.
-    em_ordem(raiz.esquerda)
-
-    # Visita nodo corrente.
-    print(raiz.produto),
-
-    # Visita filho da direita.
-    em_ordem(raiz.direita)
+                troca(lista, i, r)
+                i = r
+        elif l < acima:
+            if lista[l] > lista[i]:
+                troca(lista, i, l)
+                i = l
+            else:
+                break
+        elif r < acima:
+            if lista[r] > lista[i]:
+                troca(lista, i, r)
+                i = r
+            else:
+                break
+        else:
+            break
 
 
-# Criação do objeto de lista encadeada com os produtos
-lista_de_produtos = ListaDeProdutos()
+def heapsort(lista):
+    for j in range(len(lista)-2//2, -1, -1):
+        braco(lista, j, len(lista))
 
-# Usuário faz a inserção de produtos na lista
-lista_de_produtos.inserir('MacBook Pro', 1999)
-lista_de_produtos.inserir('Apple Vision Pro', 3499)
-lista_de_produtos.inserir('Apple Watch', 399)
-lista_de_produtos.inserir('iMac', 1699)
-lista_de_produtos.inserir('iPhone', 999)
-
-# Exibe a lista de produtos de forma não ordenada
-print(lista_de_produtos.exibir())
-
-# Inserindo elementos da lista na árvore
-raiz = Node(lista_de_produtos.retornar_valor_indice(0))
-
-for i in range(1, lista_de_produtos.tamanho()):
-    elemento = lista_de_produtos.retornar_valor_indice(i)
-    raiz.inserir(elemento)
-
-em_ordem(raiz)
+    for fim in range(len(lista)-1, 0, -1):
+        troca(lista, 0, fim)
+        braco(lista, 0, fim)
 
 
+produtos = [1999, 3499, 399, 1699, 999]
+
+heapsort(produtos)
+
+print(produtos)
